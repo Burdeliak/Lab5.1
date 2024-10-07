47adf0fa5c340bfc8fa6af4a6a5dfc3a3b6ae19d
Олександр <oleksandr140407@gmail.com>
2024-10-08 07:04:50 +1000

    Lab5.1

---
+++ UnitTest Lab5.1/UnitTest Lab5.1.cpp
@@ -0,0 +1,19 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTestLab51
+{
+	TEST_CLASS(UnitTestLab51)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+			double c = sin(1) / (pow(1, 2) + pow(1, 2)) + cos(1) / (1 + abs(1 * 1));
+			double a = 1 + c;
+			Assert::AreEqual(a, 1,69);
+		}
+	};
+}
